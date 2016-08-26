60eb0ad10257c9a7702c041c4c562b5c1a7b8239
cxMax <caixi169@hotmail.com>
2016-08-27 01:43:35 +0800

    '新增微信分享和DataBinding事件处理基类'

---
+++ app/src/main/java/com/cxmax/mvvmdemo/viewmodel/base/BaseViewModel.java
@@ -0,0 +1,11 @@
+package com.cxmax.mvvmdemo.viewmodel.base;
+
+import android.databinding.BaseObservable;
+
+/**
+ * @Author CaiXi on  2016/8/27 01:39.
+ * @Github: https://github.com/cxMax
+ * @Description DataBinding事件回调基类
+ */
+public class BaseViewModel extends BaseObservable{
+}
---
+++ app/src/main/java/com/cxmax/mvvmdemo/wxapi/WXEntryActivity.java
@@ -0,0 +1,9 @@
+package com.cxmax.mvvmdemo.wxapi;
+
+/**
+ * @Author CaiXi on  2016/8/27 01:35.
+ * @Github: https://github.com/cxMax
+ * @Description 微信分享、微信支付
+ */
+public class WXEntryActivity {
+}
