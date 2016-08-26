b507c2eb8f816fe6a5dc5040cb382caf0a2347d1
cxMax <caixi169@hotmail.com>
2016-08-27 01:33:28 +0800

    '新增EventBus基类'

---
+++ app/src/main/java/com/cxmax/mvvmdemo/utils/event/BaseEvent.java
@@ -0,0 +1,9 @@
+package com.cxmax.mvvmdemo.utils.event;
+
+/**
+ * @Author CaiXi on  2016/8/27 01:32.
+ * @Github: https://github.com/cxMax
+ * @Description EventBus事件基类
+ */
+public class BaseEvent {
+}
